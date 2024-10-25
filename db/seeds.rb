7b6a932b67ed885b8d04332f3ca752ea46183da8
mohamed abdelrahman mohamed <mohamed_abdelrahman@icloud.com>
2024-10-25 19:57:46 +0300

    seed the tax_rate table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# seed the database
+rates = Europe::Vat::Rates.retrieve
+Europe::Countries::COUNTRIES.each do |key, value|
+  [ "good", "digital_service", "onsite_service" ].each do |product_type|
+    TaxRate.create!(country: value[:name], product_type: product_type, vat_rate: rates[key])
+  end
+end
